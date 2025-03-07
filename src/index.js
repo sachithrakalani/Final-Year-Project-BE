bbe7f1204c9514db33f667e380598011ea29c6e9
sachithrakalani <kalanisathyangi@gmail.com>
2025-03-07 15:58:41 +0530

    Create Server in Fake review detection project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+const express = require("express");
+const app = express();
+
+//Create Server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+//Create Responce
+app.get("/", (req, res) => [res.send("Hello World!")]);
