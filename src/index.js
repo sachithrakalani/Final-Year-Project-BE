3ebcd81476acbf61492c8dbf8d59da1b0f5a8206
sachithrakalani <kalanisathyangi@gmail.com>
2025-03-08 14:15:35 +0530

    Create Database Connection using Postgres

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import pool from "./config/db.js";
 
 dotenv.config();
 
@@ -8,6 +9,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//Testing POSTGRES Connection
+app.get("/", async (req, res) => {
+  console.log("Start");
+  const result = await pool.query("SELECT current_database()");
+  console.log("Result", result.rows);
+  res.send(`The Database name is : ${result.rows[0].current_database}`);
+});
 
 //Create Responce
 app.get("/", (req, res) => {
