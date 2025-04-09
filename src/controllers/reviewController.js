b77b9e35e1e72ba2d0bbe2759a0aff3d9bacc10f
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-09 20:53:14 +0530

    Modify createReviews function

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,7 +1,22 @@
+import { createReviewQuery } from "../models/reviewModel.js";
+
 const handleResponse = (res, status, message, data = null) => {
   res.status(status).json({
     status,
     message,
     data,
   });
+};
+
+export const createReviews = async (req, res, next) => {
+  const { reviewtext, result } = req.body;
+  if (!reviewtext) {
+    return res.status(400).json({ error: "review_text is required" });
+  }
+  try {
+    const newReview = await createReviewQuery(reviewtext, result);
+    handleResponse(res, 200, "New Review Added Successfully", newReview);
+  } catch (error) {
+    next(error);
+  }
 };
