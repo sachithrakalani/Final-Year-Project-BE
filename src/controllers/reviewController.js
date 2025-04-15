5813933ebb5ac7ea093f0a41da4074392b34a271
sachithrakalani <kalanisathyangi@gmail.com>
2025-04-15 20:14:37 +0530

    Change varibale name in reviewController.js file

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -9,12 +9,12 @@ const handleResponse = (res, status, message, data = null) => {
 };
 
 export const createReviews = async (req, res, next) => {
-  const { reviewtext, result } = req.body;
-  if (!reviewtext) {
+  const { review} = req.body;
+  if (!review) {
     return res.status(400).json({ error: "review_text is required" });
   }
   try {
-    const newReview = await createReviewQuery(reviewtext, result);
+    const newReview = await createReviewQuery(review);
     handleResponse(res, 200, "New Review Added Successfully", newReview);
   } catch (error) {
     next(error);
